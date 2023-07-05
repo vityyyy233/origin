ef7c50b767d8845a3bd584a7eb9ccc26b6e6a640
VS <vityyyy233@gmail.com>
2023-07-05 19:55:18 +0500

    Initial commit

---
+++ src/main/java/com/example/qqqq/QqqqApplication.java
@@ -0,0 +1,13 @@
+package com.example.qqqq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqqqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqqqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qqqq/QqqqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qqqq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqqqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
